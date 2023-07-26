5fce221097b74153cc62fa576f8e210a9a8e45c9
LourdesGonzalezT <LourdesGonzalezT@github.com>
2023-07-26 12:22:15 +0200

    login in server

--- server/routes/auth.routes.js
+++ server/routes/auth.routes.js
@@ -1,7 +1,8 @@
 const router = require("express").Router()
 const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
 
-const User = require('./../models/user.model')
+const User = require('./../models/User.model')
 const saltRounds = 10
 
 router.post('/signup', (req, res, next) => {
@@ -38,5 +39,51 @@ router.post('/signup', (req, res, next) => {
             next(err)
         })
 })
+
+
+
+
+router.post('/login', (req, res, next) => {
+
+    console.log('secretoo', process.env.TOKEN_SECRET)
+
+    const { email, password } = req.body;
+
+    if (email === '' || password === '') {
+        res.status(400).json({ message: "Provide email and password." });
+        return;
+    }
+
+    User
+        .findOne({ email })
+        .then((foundUser) => {
+
+            if (!foundUser) {
+                res.status(401).json({ message: "User not found." })
+                return;
+            }
+
+            if (bcrypt.compareSync(password, foundUser.password)) {
+
+                const { _id, email, username } = foundUser;
+
+                const payload = { _id, email, username }
+
+                const authToken = jwt.sign(
+                    payload,
+                    process.env.TOKEN_SECRET,
+                    { algorithm: 'HS256', expiresIn: "6h" }
+                )
+
+                res.json({ authToken: authToken });
+            }
+            else {
+                res.status(401).json({ message: "Unable to authenticate the user" });
+            }
+
+        })
+        .catch(err => next(err));
+})
+
 
 module.exports = router
