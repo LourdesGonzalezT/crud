2504442f617939a26fc586bce058ba78a0363e05
LourdesGonzalezT <LourdesGonzalezT@github.com>
2023-07-29 10:02:37 +0200

    context login in process

--- server/routes/auth.routes.js
+++ server/routes/auth.routes.js
@@ -87,9 +87,12 @@ router.post('/login', (req, res, next) => {
 })
 
 
+router.get('/verify', isAuthenticated, (req, res, next) => {
+    console.log('EL USUARIO TIENE UN TOKEN CORRECTO Y SUS DATOS SON', req.payload)
 
-router.get('verify', isAuthenticated, (req, res, next) => {
-    console.log('EL USUARIO TIENE UN TOKEN CORRECTO Y SUS DATOS SON, req.payload')
+    setTimeout(() => {
+        res.status(200).json(req.payload)
+    }, 1500)
 })
 
 module.exports = router
